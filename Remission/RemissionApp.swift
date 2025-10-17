2800f225be404ed3eebfe4d82e783fa20ddb4736
Aleksandr Babkin <plizkinzmey@gmail.com>
2025-10-17 03:45:46 +0300

    Initial Commit

---
+++ Remission/RemissionApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemissionApp.swift
+//  Remission
+//
+//  Created by Aleksandr on 17.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct RemissionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RemissionTests/RemissionTests.swift
@@ -0,0 +1,16 @@
+//
+//  RemissionTests.swift
+//  RemissionTests
+//
+//  Created by Aleksandr on 17.10.2025.
+//
+
+import Testing
+
+struct RemissionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
