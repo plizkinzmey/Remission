ed634da2426162e5b447a29ac1c45af11701974e
Aleksandr Babkin <plizkinzmey@gmail.com>
2025-11-08 21:31:22 +0300

    Упростить создание TestStore

--- RemissionTests/Support/TestStoreFactory.swift
+++ RemissionTests/Support/TestStoreFactory.swift
@@ -6,17 +6,43 @@ import Foundation
 
 @MainActor
 enum TestStoreFactory {
-    /// Создаёт TestStore с дефолтным набором зависимостей из AppDependencies.
+    /// Собирает базовый набор зависимостей, используемый во всех тестах:
+    /// `appClock`, `mainQueueExecutor`, `dateProvider`, `uuidGenerator` и `transmissionClient`.
+    private static func configureDefaults(in dependencies: inout DependencyValues) {
+        dependencies = AppDependencies.makeTestDefaults()
+    }
+
+    /// Универсальный билд `TestStore` для любого редьюсера проекта.
     static func make<R: Reducer>(
         initialState: @autoclosure () -> R.State,
         reducer: @escaping () -> R,
         configure: @Sendable (inout DependencyValues) -> Void = { _ in }
     ) -> TestStoreOf<R> {
-        TestStore(initialState: initialState()) {
+        let store = TestStore(initialState: initialState()) {
             reducer()
         } withDependencies: { dependencies in
-            dependencies = AppDependencies.makeTestDefaults()
+            configureDefaults(in: &dependencies)
             configure(&dependencies)
         }
+        store.exhaustivity = .off
+        return store
+    }
+
+    /// Упрощённая фабрика для `AppReducer`, избавляющая от дублирования state/reducer и ветки `withDependencies`.
+    static func makeAppTestStore(
+        initialState: @autoclosure () -> AppReducer.State = .init(),
+        reducer: @escaping () -> AppReducer = { AppReducer() },
+        configure: @Sendable (inout DependencyValues) -> Void = { _ in }
+    ) -> TestStoreOf<AppReducer> {
+        make(initialState: initialState(), reducer: reducer, configure: configure)
+    }
+
+    /// Упрощённая фабрика для `ServerListReducer`, чтобы сократить boilerplate в фичевых тестах.
+    static func makeServerListTestStore(
+        initialState: @autoclosure () -> ServerListReducer.State = .init(),
+        reducer: @escaping () -> ServerListReducer = { ServerListReducer() },
+        configure: @Sendable (inout DependencyValues) -> Void = { _ in }
+    ) -> TestStoreOf<ServerListReducer> {
+        make(initialState: initialState(), reducer: reducer, configure: configure)
     }
 }
